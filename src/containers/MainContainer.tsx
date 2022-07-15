e531d77ebdb3a811cb97cc76a2b5dea5a01f2cbe
cteyr123 <cteyr123@gmail.com>
2022-07-15 13:18:36 +0200

    api configuration finished

--- src/containers/MainContainer.tsx
+++ src/containers/MainContainer.tsx
@@ -1,4 +1,27 @@
+import { api } from "../api/api";
+import { useState, useEffect } from "react";
+import { Product } from "../types/product";
+
 const MainContainer = () => {
+  const [Products, setProducts] = useState<Product[]>([]);
+
+  const handleResponse = async () => {
+    const { response, error } = await api.getProducts();
+    // setIsLoading(false);
+    if (error) {
+      //setError(error);
+      //setvisibleAlert(true);
+      // setIsLoading(false);
+    } else {
+      //setvisibleAlert(false);
+      setProducts((prev) => [response, ...prev]);
+    }
+  };
+
+  useEffect(() => {
+    handleResponse();
+  }, []);
+
   return <div className="maincontainer">Hola mundo</div>;
 };
 
